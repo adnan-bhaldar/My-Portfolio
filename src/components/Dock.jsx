f9998f748044acb61bbb5884f3fd54c1da875f70
adnan-bhaldar <adnanbhaldar115@gmail.com>
2025-12-03 10:42:58 +0530

    window validation

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,10 +1,12 @@
-import { dockApps } from '#constants';
+import { dockApps } from '#constants/index.js';
 import { useGSAP } from '@gsap/react';
 import React, { useRef } from 'react'
 import { Tooltip } from 'react-tooltip'
 import gsap from 'gsap';
+import useWindowStore from '#store/window.js';
 
 const Dock = () => {
+    const { openWindow, closeWindow, windows} = useWindowStore()
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -57,6 +59,22 @@ const Dock = () => {
 
 
     const toggleApp = (app) => {
+        if(!app. canOpen) return;
+
+        const window = windows[app.id];
+
+        if(!window) {
+            console.error(`Window not found for app: ${app.id}`);
+            return;
+        }
+
+        if(window.isOpen) {
+            closeWindow(app.id);
+        } else {
+            openWindow(app.id);
+        }
+
+        console.log(windows);
         
     }
 
