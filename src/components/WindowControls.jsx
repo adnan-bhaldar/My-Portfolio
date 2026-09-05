22b191c9da6e6efa7453b425235f53d020817721
adnan-bhaldar <adnanbhaldar115@gmail.com>
2026-09-05 11:01:21 +0530

    feat: show close/minimize/maximize icons on traffic light hover

--- src/components/WindowControls.jsx
+++ src/components/WindowControls.jsx
@@ -5,9 +5,15 @@ const WindowControls = ({ target }) => {
     const { closeWindow } = useWindowStore();
     return (
         <div id='window-controls'>
-            <div className='close' onClick={() => closeWindow(target)} />
-            <div className='minimize' />
-            <div className='maximize' />
+            <div className='close' onClick={() => closeWindow(target)}>
+                <img src="/icons/window-close.svg" alt="Close" />
+            </div>
+            <div className='minimize'>
+                <img src="/icons/window-minimize.svg" alt="Minimize" />
+            </div>
+            <div className='maximize'>
+                <img src="/icons/window-maximize.svg" alt="Maximize" />
+            </div>
         </div>
     )
 }
