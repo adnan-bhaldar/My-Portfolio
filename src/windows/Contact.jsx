3e3e4cbaeb534342a016c5c26bc4279b6520d91c
adnan-bhaldar <adnanbhaldar115@gmail.com>
2026-09-08 20:53:06 +0530

    fix(contact): remove native title attribute causing default browser tooltip on social links

--- src/windows/Contact.jsx
+++ src/windows/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
             <ul>
                 {socials.map(({id, bg, link, icon, text}) => (
                     <li key={id} style={{backgroundColor: bg}}>
-                        <a href={link} target='_blank' rel='noopener noreferrer' title={text}>
+                        <a href={link} target='_blank' rel='noopener noreferrer'>
                             <img src={icon} alt={text} className='size-5' />
                             <p>{text}</p>
                         </a>
